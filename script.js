bd6a6b9f27e9ecfaeaf879d299b8b526c9408201
JenWinner <jmascarenhaswinner@gmail.com>
2019-06-11 17:06:26 -0300

    Lacos

---
+++ script.js
@@ -0,0 +1,5 @@
+let signos = ["aries", "aquario", "cancer", "peixes", "capricornio"];
+
+for(let valores in signos){
+    console.log(" A palavra no indice"+" " + valores +" "+ "é: "+ signos[valores]);
+}
